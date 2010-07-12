eb84d03b131bbc37b639367fb1c6280673060168
Diogenes <nenco.jpa@gmail.com>
2010-07-12 19:48:52 -0300

    Removendo arquivos do netbeans

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,9 +1,7 @@
 class Produto < ActiveRecord::Base
 
   #set_table_name 'produto' //setar o nome da tabela
-
   validates_presence_of :nome, :preco
   validates_numericality_of :preco, :great_than => 0, :allow_nil => true
-
   
 end
