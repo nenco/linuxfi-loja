4d08deb492cd01e0f8382c290d738919e45d95f5
Diogenes <nenco.jpa@gmail.com>
2010-07-14 22:01:31 -0300

    Aula_04 atualizar carrinho

--- app/controllers/itens_controller.rb
+++ app/controllers/itens_controller.rb
@@ -2,15 +2,27 @@ class ItensController < ApplicationController
 
   def create
     @produto = Produto.find( params[:produto_id] )
+
     pedido_atual.adicionar_produto(@produto, params[:quantidade].to_i )
     pedido_atual.save
 
     session[:pedido_id] = pedido_atual.id
-    
+    ir_para_carrinho("Produto #{@produto.nome} adcionado com sucesso ao carrinho")
+  end
+
+
+  def atualizar_pedido
+    pedido_atual.update_attributes( params[:pedido])
+    ir_para_carrinho("Carrinho atualizado com sucesso")
+  end
+
+  protected
+
+  def ir_para_carrinho (mensagem )
     respond_to do |format|
       format.html do
-        flash[:aviso] = "Produto #{@produto.nome} adicionado com sucesso ao carrinho"
-        redirect_to produtos_path
+        flash[:aviso] = mensagem
+        redirect_to itens_path
       end
     end
   end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,6 +1,8 @@
 class Pedido < ActiveRecord::Base
 
-  has_many :itens
+  has_many :itens, :dependent => :destroy
+  accepts_nested_attributes_for :itens
+  after_save :remover_itens_zerados
 
   def adicionar_produto( produto, quantidade)
     if item = self.itens.detect { |i| i.produto == produto}
@@ -8,6 +10,27 @@ class Pedido < ActiveRecord::Base
     else
       self.itens.build( :produto_id => produto.id, :quantidade => quantidade )
     end
+  end
+
+  def preco_total
+    self.itens.to_a.sum( &:preco_total )
+  end
+
+  def blank?
+    self.itens.blank?
+  end
+
+  protected
+
+  def remover_itens_zerados
+    itens_a_remover = []
+    self.itens.each do |item|
+      if item.quantidade.blank? || item.quantidade < 1
+        itens_a_remover << item
+      end
+    end
+    self.itens.delete( *itens_a_remover)
+    true
   end
 
 end
