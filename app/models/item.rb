0330eeadee2f366aba1476f6e9785c5ff13d4c27
Diogenes <nenco.jpa@gmail.com>
2010-08-09 20:16:04 -0300

    Adicionando validação de número em quantidade e itens

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :produto
   belongs_to :pedido
 
+  validates_numericality_of :quantidade
+
   def preco_total
     self.quantidade * self.produto.preco
   end
