e323836b824ee124aaf8c47d8372c2854da6407b
Diogenes <nenco.jpa@gmail.com>
2010-08-25 21:43:58 -0300

    e-mail

--- config/initializers/email_config.rb
+++ config/initializers/email_config.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true,
-  :address => "smtp,gmail.com",
+  :address => "smtp.gmail.com",
   :port => 587,
   :domain => "gmail.com",
   :authentication => :plain,
